f9ced8b4687145da754e4060d45c255072cd558f
Jim Fitzpatrick <jfitzpat@redhat.com>
2026-04-07 11:49:22 +0100

    update command
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- functions/va.fish
+++ functions/va.fish
@@ -1,4 +1,16 @@
 function va
-  source venv/bin/activate.fish
+
+  set -l root venv
+
+  if test -e venv
+    set root venv
+  else if test -e .venv
+    set root .venv
+  else
+    echo "no venv found"
+    return
+  end
+    
+  source $root/bin/activate.fish
 
 end
