5410981fe5caa5d281dd2f73f17ea502880c74c3
Jim Fitzpatrick <jfitzpat@redhat.com>
2026-04-15 16:55:53 +0100

    update: gen_pr function
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- functions/gen_pr.fish
+++ functions/gen_pr.fish
@@ -1,14 +1,19 @@
 function gen_pr
+    argparse 'p/prompt=' -- $argv
+    or return 1
+
     set -l branch $argv[1]
     set -l file_loc "/tmp/pr.md"
 
-    # If the variable 'name' is not set or is empty
     if test -z "$branch"
         set branch "main"
     end
-    set -l message "review the changes between this branch and $branch. Come up with a PR title and description"
+
+    set -l message "review the changes between this branch and $branch. Come up with a PR title and description."
+    if set -q _flag_prompt
+        set message "$message\n$_flag_prompt"
+    end
+
     claude -p $message > $file_loc
-
     nvim $file_loc
-
 end
